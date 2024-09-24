40c1f7435a6bf8e89a8108430cde8d1242ee8b76
blvck-root <mtndlovu81@gmail.com>
2024-09-24 12:51:55 +0200

    added routes for fetching all users and deleting a specific user by id

--- backend/blueprints/main_blueprint.py
+++ backend/blueprints/main_blueprint.py
@@ -1,5 +1,19 @@
+import json
+import base64
+
 from flask import Blueprint, request, render_template
 from models.user import User
+from bson import ObjectId
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        if isinstance(o, bytes):
+            return base64.b64encode(o).decode('utf-8')
+        return super().default(o)
+
 
 main_views = Blueprint("main", __name__)
 
@@ -11,3 +25,21 @@ def index():
 def profile(username):
     user = User.find_by_username(username)
     return render_template("profile.html", user=user)
+
+@main_views.get("/users", strict_slashes=False)
+def get_users():
+    users = User.find_all_users()
+    return JSONEncoder().encode(users)
+
+@main_views.delete("/users/<user_id>", strict_slashes=False)
+def delete_user_by_id(user_id):
+    result = User.collection.find_one_and_delete({'_id': ObjectId(user_id)})
+
+    if result:
+        print(result)
+        return JSONEncoder().encode(
+            {'message': "User deleted successfully"}
+        ), 200
+    return JSONEncoder().encode(
+        {'message': "User not found"}
+    ), 200
